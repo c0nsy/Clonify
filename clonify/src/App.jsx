d293113d255ca7ab9729b7c5969d383f4cf557b4
Connor Logan <connorlogan00@gmail.com>
2022-07-31 19:55:27 -0400

    tutorial work

--- clonify/src/App.jsx
+++ clonify/src/App.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import './App.css'
-import Navbar from './components/Navbar'
+import Navbar from './Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
