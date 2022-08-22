8b53e2e5556ab10a45bfbcba7286a2b6baaaef7d
Connor Logan <connorlogan00@gmail.com>
2022-08-22 12:41:12 -0400

    refractoring code to add functionality between siblings

---
+++ clonify/src/components/MainCardContainer.jsx
@@ -0,0 +1,34 @@
+import React from "react"
+import Data from "../data"
+
+function MainCardContainer(){
+    const mainCards = Data.map(card => {
+        return(
+          <MainCard 
+              key={card.id}
+              id={card.id}
+              image={card.url}
+              title={card.title}
+              playbutton={card.playbutton}
+              addbutton={card.addbutton}
+          />
+        )
+      })
+    const handleAdd = (title) => {
+        setPlaylistItem(title);
+    }
+    return(
+        <div>
+            {mainCards}
+            {/* <div className="mainCardObject">
+                <div className="cardObj">
+                    <img src={props.image} className ="mainCardImage"/>
+                    <img src={props.playbutton} className="playbutton"/>
+                    <img src={props.addbutton} onClick={() => handleAdd(props.id)} className="addbutton" />
+                </div>
+            </div> */}
+        </div>
+        
+    )
+}
+export default MainCardContainer
